132cca9a43f4400309c186031940e304f820d6b8
stepan-khachatryan <stepan.k.g@tumo.org>
2023-06-28 16:53:19 +0100

    Add more charts

---
+++ Session 1/scatterPlot.py
@@ -0,0 +1,15 @@
+import random
+import matplotlib.pyplot as plt
+
+random.seed(42)
+x = [random.random() for _ in range(50)]
+y = [random.random() for _ in range(50)]
+colors = [random.random() for _ in range(50)]
+sizes = [random.randint(10, 100) for _ in range(50)]
+
+plt.scatter(x, y, c = colors, s = sizes, marker = 'o')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('Scatter Plot')
+plt.grid(True)
+plt.show()
